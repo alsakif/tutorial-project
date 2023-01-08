9e5eaf218888b7910921e4d1aa000fc6395bb197
alsakif <alsakif1.618@gmail.com>
2023-01-08 09:59:40 +0600

    Lesson completed

--- src/components/lessons/06/lesson-06.tsx
+++ src/components/lessons/06/lesson-06.tsx
@@ -14,7 +14,7 @@ export const Lesson06: React.FC = () => {
     return (
         <div className={styles.root}>
             <Task />
-            <div className={`${styles.playground}`} id="playground">
+            <div className={`${styles.playground} ${styles.classless}`} id="playground">
                 <div className={styles.quote}>
                     “Creativity is <span>nothing but a mind set</span> free.”
                 </div>
